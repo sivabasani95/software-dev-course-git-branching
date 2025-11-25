359ae23d0455b86201e144e68f29e2460f3e7985
sivabasani95 <sivabasani95@gmail.com>
2025-11-25 14:45:37 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+// siva basani 
+
 console.log("Hello world!");
 
 for (let i = 0; i < 10; i++) {
     console.log(i);
+     console.log(i * i);   // prints the square of that number (0, 1, 4, 9, ...)
+}
 }
